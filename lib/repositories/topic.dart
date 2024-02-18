ce8b497c05730b89b038d6e4cf8b958fd7669a64
namooplus <namoo.developer@gmail.com>
2024-02-19 01:13:12 +0900

    feat: add dio

--- lib/repositories/topic.dart
+++ lib/repositories/topic.dart
@@ -1,12 +1,16 @@
+import 'dart:convert';
+
+import 'package:dio/dio.dart';
 import 'package:fluentify/data/topics.dart';
 import 'package:fluentify/interfaces/topic.pb.dart';
 
 class TopicRepository {
-  // TODO: API 연동
-  Future<ListTopicsResponse> listTopics(ListTopicsRequest request) async {
-    await Future.delayed(const Duration(seconds: 1));
+  final Dio dio = Dio(BaseOptions(baseUrl: 'http://api.fluentify.dev'));
 
-    return ListTopicsResponse(topics: sampleCompactTopics);
+  Future<ListTopicsResponse> listTopics(ListTopicsRequest request) async {
+    final response = await dio.post('/ListTopics');
+
+    return ListTopicsResponse.fromJson(json.encode(response.data));
   }
 
   // TODO: API 연동
