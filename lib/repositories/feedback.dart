a054e86204ebcf6ff59285784b0d920818da5b69
namooplus <namoo.developer@gmail.com>
2024-02-21 18:46:26 +0900

    feat: use snake case for feedback request

--- lib/repositories/feedback.dart
+++ lib/repositories/feedback.dart
@@ -9,7 +9,11 @@ class FeedbackRepository {
 
     final response = await client.post(
       '/GetPronunciationFeedback',
-      data: request.toProto3Json(),
+      // data: request.toProto3Json(),
+      data: {
+        'sentence_id': request.sentenceId,
+        'audio_file_url': request.audioFileUrl,
+      },
     );
 
     return GetPronunciationFeedbackResponse.create()
@@ -23,7 +27,11 @@ class FeedbackRepository {
 
     final response = await client.post(
       '/GetCommunicationFeedback',
-      data: request.toProto3Json(),
+      // data: request.toProto3Json(),
+      data: {
+        'scene_id': request.sceneId,
+        'audio_file_url': request.audioFileUrl,
+      },
     );
 
     return GetCommunicationFeedbackResponse.create()
